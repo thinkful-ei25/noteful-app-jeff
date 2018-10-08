ba88a8219a3e3301c77f54bc53a9a47b640d1016
jeff oriente <jdanieloriente@gmail.com>
2018-10-08 14:54:14 -0400

    Did noteful app

--- server.js
+++ server.js
@@ -6,3 +6,18 @@ const data = require('./db/notes');
 console.log('Hello Noteful!');
 
 // INSERT EXPRESS APP CODE HERE...
+const express = require('express');
+
+// const data = require('./db/notes');
+
+const app = express();
+
+// ADD STATIC SERVER HERE
+app.use(express.static('public'));
+/////////////////////////
+
+app.listen(8080, function () {
+  console.info(`Server listening on 8080`);
+}).on('error', err => {
+  console.error(err);
+});
